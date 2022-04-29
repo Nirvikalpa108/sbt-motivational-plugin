fcbed7e3dc59a3de1cb27609662620b968232062
Amina Adewusi <amina.adewusi@permutive.com>
2022-04-29 05:53:14 +0100

    complete MVP plugin

--- src/main/scala/com/github/Nirvikalpa108/Plugin.scala
+++ src/main/scala/com/github/Nirvikalpa108/Plugin.scala
@@ -4,25 +4,25 @@ import sbt.AutoPlugin
 import sbt._
 
 object MotivationPlugin extends AutoPlugin {
-
+  // this specifies the plugins that I need to depend on. Autoplugin puts the plugin dependency settings in the right
+  // order for us. This has to be done manually before 0.13.5.
   override def requires = sbt.plugins.CorePlugin
+  // this means the plugin is automatically added to the project's which use it. no manual trigger is required.
   override def trigger = allRequirements
 
   object autoImport {
-    val motivations: SettingKey[List[String]] = settingKey[List[String]]("a list of motivational quotes")
+    val motivationalQuotes: SettingKey[List[String]] = settingKey[List[String]]("a list of motivational quotes")
+    val speak = taskKey[Unit]("says nice motivational things")
   }
 
   import autoImport._
-  override lazy val globalSettings: Seq[Setting[_]] = List(motivations := List(
-  "Take a deep breath in and out",
-  "You birthed a baby completely by yourself, you can do anything",
-  "You took Zak through sleep training when everyone was against you, you can do anything",
-  "You barely slept for the first 5 months of Zak’s life. This is easy compared to that.",
-  "You can achieve anything you want, just take it step by step",
-  "Where you are is where you are meant to be",
-  "You have everything you need to look after yourself",
-  "I believe in you",
+  import sys.process._
+  override lazy val globalSettings: Seq[Setting[_]] = List(
+    motivationalQuotes := MotivationalQuotes.quotes,
+    speak := {
+      val m = motivationalQuotes.value
+      val name = System.getProperty("user.name")
+      Process(s"say ${m.head} $name").!!
+    }
   )
-  )
-
 }
