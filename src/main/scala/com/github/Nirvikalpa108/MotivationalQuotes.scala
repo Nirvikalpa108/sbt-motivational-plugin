f2ed69b760ffb830169b0ec810ab37ded9140817
Amina Adewusi <amina.adewusi@permutive.com>
2022-04-29 05:52:14 +0100

    move the quotes out into another file

---
+++ src/main/scala/com/github/Nirvikalpa108/MotivationalQuotes.scala
@@ -0,0 +1,21 @@
+package com.github.Nirvikalpa108
+
+object MotivationalQuotes {
+  val quotes = List(
+    "Take a deep breath in and out",
+    "You birthed a baby completely by yourself, you can do anything",
+    "You took Zak through sleep training when everyone was against you, you can do anything",
+    "You barely slept for the first 5 months of Zak’s life. This is easy compared to that.",
+    "You can achieve anything you want, just take it step by step",
+    "Where you are is where you are meant to be",
+    "You have everything you need to look after yourself",
+    "I believe in you",
+    "You can achieve whatever you choose",
+    "You are enough, just as you are",
+    "You know where you are headed",
+    "Keep doing what you are doing, you will get to where you want to be",
+    "You don't have to know it all now. Learning is part of the fun",
+    "The reality is now",
+    "We are nothing but the stories we tell ourselves",
+  )
+}
