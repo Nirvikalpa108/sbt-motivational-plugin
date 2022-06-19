e553bd4e93ad6557dac44c4063e1df882cbfd897
Amina Adewusi <amina.adewusi@permutive.com>
2022-06-19 19:53:10 +0100

    voice by sub project working

--- src/main/scala/com/github/Nirvikalpa108/Plugin.scala
+++ src/main/scala/com/github/Nirvikalpa108/Plugin.scala
@@ -36,29 +36,6 @@ object MotivationPlugin extends AutoPlugin {
     voice := "Daniel", // voice is set to a default value in global settings
   )
   override lazy val projectSettings: Seq[Setting[_]] = List(
-    // created sub projects
-    // the tests are in the root of the project
-    // they run no matter which sub project I am in
-    // TODO: if I have tests in a sub-project, they're only run when I'm in that sub-project right?
-
-    // when I'm in a sub-project, the right voice is used for the test
-    // when I'm not in any sub-project (in the root)
-
-    // very interesting ...
-    // when I have only a sub project called root (is that a sub project or the project??!) -
-    // I hear the global fallback AND the voice set for the root -
-    // BUT when I have a second sub-project (or first, depending on whether root is a sub project) -
-    // I only hear the voice for root and the second sub-project, NOT the global fall back
-    // TODO: Is root sub project a reserved key word / means something?
-
-    // when I'm in the root sub project, AND the tests are in the root, All of the sub-projects tests run in parallel
-    // and therefore all of the voices are heard in parallel
-    // TODO what can I do? Should I put the tests in the sub-projects? Will this counter that?
-
-    // ***** IMPORTANT, EXPECTED BEHAVIOUR HERE *****
-    // when I'm in a sub-project AND the tests are in the root, I only hear the sub-project voice
-    // when I'm in a sub-project AND no voice has been set in the sub-proj AND the tests are in the root, I hear the global fallback set by the plugin
-
     //setting the voice to the narrowest scoping within the tasks, so build users have max flexibility and can set per sub-project
     speakTestPassed := {
       val v = (speakTestPassed / voice).value
