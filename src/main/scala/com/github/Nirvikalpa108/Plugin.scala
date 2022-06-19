b6466a2befb7824393bbdc88c333138ef0141fa4
Amina Adewusi <amina.adewusi@permutive.com>
2022-06-19 15:10:36 +0100

    WIP - sub-project voices working

--- src/main/scala/com/github/Nirvikalpa108/Plugin.scala
+++ src/main/scala/com/github/Nirvikalpa108/Plugin.scala
@@ -15,7 +15,7 @@ object MotivationPlugin extends AutoPlugin {
     //val motivationalQuotes: SettingKey[List[String]] = settingKey[List[String]]("a list of motivational quotes")
     //val voices: SettingKey[List[String]] = settingKey[List[String]]("a list of say voices")
     val speak = taskKey[Unit]("says nice motivational things")
-//    val voice = settingKey[String]("configure the voice") // voice that the user can set per sub-project
+    val voice = settingKey[String]("configure the voice") // voice that the user can set per sub-project
     val speakTestPassed = taskKey[Unit]("say something nice when the tests pass")
     val speakTestFailed = taskKey[Unit]("say something motivational when the tests fail")
     val speakTestError = taskKey[Unit]("say something motivational when the tests error")
@@ -28,69 +28,55 @@ object MotivationPlugin extends AutoPlugin {
         case TestResult.Error => Def.task(speakTestError.value)
       }
     }
-
-//    val dynamic = Def.taskDyn {
-//      // decide what to evaluate based on the value of Test / executeTests
-//      if((Test / executeTests).value.overall == TestResult.Failed)
-//      // this is only evaluated if the tests fail
-//        Def.task {
-//          speakTestFailed.value
-//        }
-//      else
-//      // only evaluated if the tests do not fail (this is wrong, because of course they could error, but anyway!)
-//        Def.task {
-//          speakTestPassed.value
-//        }
-//    }
   }
 
   import autoImport.*
   import sys.process.*
   override lazy val globalSettings: Seq[Setting[_]] = List(
-
-    //voice := "Daniel", // voice is set to a default value in global settings
-//    motivationalQuotes := MotivationalQuotes.quotes,
-//    voices := Voices.allVoices,
-//    speak := {
-//      val m = motivationalQuotes.value
-//      val v = voices.value
-//      val random = new Random
-//      val name = System.getProperty("user.name")
-//      // I don't like these voices I've chosen, so leaving this for now until I have time to change them
-//      //Process(s"say -v ${v(random.nextInt(v.length))} ${m(random.nextInt(m.length))} $name").!!
-//      Process(s"say ${m(random.nextInt(m.length))} $name").!!
-//    },
+    voice := "Daniel", // voice is set to a default value in global settings
   )
   override lazy val projectSettings: Seq[Setting[_]] = List(
-    // execute speak in the sbt shell to see this working
-    speakTestFailed := Process("say try again. You'll have better luck next time").!!,
-    speakTestPassed := Process("say well done, your tests passed").!!,
-    speakTestError := Process("say oops something went wrong. You're still amazing though!").!!,
+    // created sub projects
+    // the tests are in the root of the project
+    // they run no matter which sub project I am in
+    // TODO: if I have tests in a sub-project, they're only run when I'm in that sub-project right?
 
+    // when I'm in a sub-project, the right voice is used for the test
+    // when I'm not in any sub-project (in the root)
+
+    // very interesting ...
+    // when I have only a sub project called root (is that a sub project or the project??!) -
+    // I hear the global fallback AND the voice set for the root -
+    // BUT when I have a second sub-project (or first, depending on whether root is a sub project) -
+    // I only hear the voice for root and the second sub-project, NOT the global fall back
+    // TODO: Is root sub project a reserved key word / means something?
+
+    // when I'm in the root sub project, AND the tests are in the root, All of the sub-projects tests run in parallel
+    // and therefore all of the voices are heard in parallel
+    // TODO what can I do? Should I put the tests in the sub-projects? Will this counter that?
+
+    // ***** IMPORTANT, EXPECTED BEHAVIOUR HERE *****
+    // when I'm in a sub-project AND the tests are in the root, I only hear the sub-project voice
+    // when I'm in a sub-project AND no voice has been set in the sub-proj AND the tests are in the root, I hear the global fallback set by the plugin
+
+    //setting the voice to the narrowest scoping within the tasks, so build users have max flexibility and can set per sub-project
+    speakTestPassed := {
+      val v = (speakTestPassed / voice).value
+      Process(s"say -v $v well done, your tests passed").!!
+    },
+    speakTestFailed := {
+      val v = (speakTestFailed / voice).value
+      Process(s"say -v $v try again, better luck next time").!!
+    },
+    speakTestError := {
+      val v = (speakTestError / voice).value
+      Process(s"say -v $v oh no, there's been an error with your tests. Let's see what's wrong.").!!
+    },
+
+    // execute speak in the sbt shell to see this working
     speak := {
       val output = speakTestOutcomeDynamic.value
       output
     }
-//    speakTest := speakTestOutcomeDynamic.value,
-
-//    Test / executeTests := { // := is a re-wiring operator
-//      val old = (Test / executeTests).value // this makes the happens-before relationship to the original task
-//      speak.value // happens before for the speak task
-//      old
-//    },
-    // setting the voice to the narrowest scoping within the tasks, so build users have max flexibility and can set per sub-project
-//    speakTestPassed := {
-//      val v = (speakTestPassed / voice).value
-//      Process(s"say -v $v well done, your tests passed").!!
-//    },
-//    speakTestFailed := {
-//      val v = (speakTestFailed / voice).value
-//      Process(s"say -v $v try again, better luck next time").!!
-//    },
-//    speakTestError := {
-//      val v = (speakTestError / voice).value
-//      Process(s"say -v $v oh no, there's been an error with your tests. Let's see what's wrong.").!!
-//    },
-//
   )
 }
